2b7b1f65df46b0fbeb64f69222afa81ff7f67a83
Alvaro Sanchez-Mariscal <alvaro.sanchezmariscal@gmail.com>
2014-03-27 04:00:37 +0100

    Customizing default renderers

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,27 @@
+import grails.rest.render.json.JsonCollectionRenderer
+import grails.rest.render.json.JsonRenderer
+import restful.grails.springsecurity.greach2014.Game
+import restful.grails.springsecurity.greach2014.Category
+
 // Place your Spring DSL code here
 beans = {
+
+    gamesRenderer(JsonCollectionRenderer, Game) {
+        excludes = ['class']
+    }
+
+    gameRenderer(JsonRenderer, Game) {
+        excludes = ['class']
+    }
+
+    categoriesRenderer(JsonCollectionRenderer, Category) {
+        excludes = ['class']
+    }
+
+    categoryRenderer(JsonRenderer, Category) {
+        excludes = ['class']
+    }
+
+
+
 }
